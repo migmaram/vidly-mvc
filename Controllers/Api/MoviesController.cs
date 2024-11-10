6fd4bb22cd968f468f51909a79ae7105143fe146
migmaram <miguel.mzrz.93@gmail.com>
2024-11-10 17:06:32 -0600

    Add MoviesController (Controller based API) and MovieDTO

---
+++ Controllers/Api/MoviesController.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Data.Entity;
+using Vidly.DTOs;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        public MoviesController(IMapper mapper)
+        {
+            _context = new ApplicationDbContext();
+            _mapper = mapper;
+        }
+        // GET api/movies
+        public async Task<ActionResult<IEnumerable<MovieDTO>>> GetMovies() 
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return Ok(_mapper.Map<List<MovieDTO>>(movies));
+        }
+        // GET api/movies/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MovieDTO>> GetMovies(int id)
+        {
+            var movie = await _context.Movies.SingleOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<MovieDTO>(movie));
+        }
+        // POST api/movies/
+        [HttpPost]
+        public async Task<IActionResult> PostMovies(MovieDTO movieDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movie = _mapper.Map<Movie>(movieDTO);
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+
+            movieDTO.Id = movie.Id;
+
+            return Created(new Uri($"{Request.GetEncodedUrl()}/{movieDTO.Id}"), new { id = movieDTO.Id, movieDTO });
+        }
+        // PUT api/movies/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovies(int id, MovieDTO movieDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movieInDb = await _context.Movies.SingleOrDefaultAsync(m => m.Id == id);
+            if (movieInDb == null)
+                return NotFound();
+
+            _mapper.Map(movieDTO, movieInDb);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        // PUT api/movies/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovies(int id)
+        {
+            var movieInDb = await _context.Movies.SingleOrDefaultAsync(m => m.Id == id);
+            if (movieInDb == null)
+                return NotFound();
+            
+            _context.Movies.Remove(movieInDb);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ DTOs/MovieDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class MovieDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+        [Required]
+        [Display(Name = "Release date")]
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        [Display(Name = "Number in stock")]
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
