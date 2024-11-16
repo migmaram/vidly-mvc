ab7cda0f6f9f0ddc7959579856a2a681e5ed4738
migmaram <miguel.mzrz.93@gmail.com>
2024-11-16 17:57:24 -0600

    Delete previous migration files to reset database

--- DTOs/MovieDTO.cs
+++ DTOs/MovieDTO.cs
@@ -8,7 +8,7 @@ namespace Vidly.DTOs
         public int Id { get; set; }
         [Required]
         [StringLength(255)]
-        public string Name { get; set; }
+        public required string Name { get; set; }
         [Required]
         [Display(Name = "Genre")]
         public byte GenreId { get; set; }
